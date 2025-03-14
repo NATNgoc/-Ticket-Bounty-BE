2715633ebb28c9e8ede777ec5f28f726cba89ac4
Tuấn Ngọc <nguyenatn2003@gmail.com>
2025-03-14 14:34:15 +0700

    Fix casing of user property in FindAllResponseDto

--- src/modules/ticket/dto/response/find-all-response.dto.ts
+++ src/modules/ticket/dto/response/find-all-response.dto.ts
@@ -1,7 +1,7 @@
 import { PartialType } from "@nestjs/mapped-types";
-import { Ticket } from "../../entities/ticket.entity";
 import { User } from "src/modules/auth/entities/user.entity";
+import { Ticket } from "../../entities/ticket.entity";
 
 export class FindAllResponseDto extends PartialType(Ticket) {
-    User: Partial<User>
+    user: Partial<User>
 }
